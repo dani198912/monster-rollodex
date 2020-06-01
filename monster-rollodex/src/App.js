f6c7db10947330297262ba7be5471e6533b86df6
dani198912 <bdaniel.mihalcea@gmail.com>
2020-06-01 10:19:05 +0300

    Added files

--- monster-rollodex/src/App.js
+++ monster-rollodex/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {CardList} from './components/card-list/cardlist.component'
+import { SearchBar } from './components/searchbar/searchbar.component'
 import './App.css';
 
 class App extends Component {
@@ -7,34 +8,42 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: [
-        {
-          name: 'Banshee',
-          id : 'asc1'
-        },
-        {
-          name: 'Dracula',
-          id : 'asc2'
-        },
-        {
-          name: 'Zombie',
-          id : 'asc3'
-        }
-      ]
+      monsters: [],
+      searchField: ''
     };
+    // this.handleChange = this.handleChange.bind(this);
   }
+
 componentDidMount(){
   fetch('https://jsonplaceholder.typicode.com/users')
   .then(response => response.json())
   .then(users => this.setState({monsters : users}))
 }
-  render() {
+
+handleChange= (e) => {//Arrow functions automaticcaly to set when it is defined, when sees the this keyword and it is binds to the place is defined
+  // in this case to the App function. This way we dont need to define in the constructor the function to bind it to app function.
+  this.setState({searchField: e.target.value})
+}
+
+render() {
+    const { monsters, searchField } = this.state;
+    //The upper line is equivalent to the next 2 lines - shorter and easier to read
+    // const monsters = this.state.monsters;
+    // const searchField = this.state.searchField;
+    const filteredMonsters = monsters.filter(monster => 
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    )
+
   return (
     <div className = "App">
-      <CardList monsters = {this.state.monsters}>
       
-      </CardList>
+      <SearchBar 
+        placeholder='Search monsters'
+        handleChange = {this.handleChange}// in object you insert a function
+      /> 
       
+      <CardList monsters = {filteredMonsters}
+      />
     </div>
   );
   }
