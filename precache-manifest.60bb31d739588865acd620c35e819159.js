36022ca2736d9748d9d50c262ad80adfd59d6a2f
dani198912 <bdaniel.mihalcea@gmail.com>
2020-06-01 11:36:31 +0300

    Updates

---
+++ precache-manifest.60bb31d739588865acd620c35e819159.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c87883e0c511d860265a8889b9ba4636",
+    "url": "/monster-rollodex/index.html"
+  },
+  {
+    "revision": "5e4d97bd69ce65f03ffc",
+    "url": "/monster-rollodex/static/css/main.aa4081ff.chunk.css"
+  },
+  {
+    "revision": "e4cfa178a7b73f3a3d39",
+    "url": "/monster-rollodex/static/js/2.289458fa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rollodex/static/js/2.289458fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e4d97bd69ce65f03ffc",
+    "url": "/monster-rollodex/static/js/main.5a0d73ff.chunk.js"
+  },
+  {
+    "revision": "239fc18b80e4b39ec3eb",
+    "url": "/monster-rollodex/static/js/runtime-main.234bb251.js"
+  }
+]);
